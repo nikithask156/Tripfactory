558526272f6a222ca87b42c5202efaef6ad7634b
nkashyap8 <nkashyap8@15.145.36.198>
2019-03-07 10:26:20 +0530

    Commit

---
+++ features/Application.feature
@@ -0,0 +1,7 @@
+Feature: Login to Facebook
+Scenario: Testing Facebook login scernario 
+Given : User is already on Login page
+When : Title of Loginpage is facebook
+Then : User enter username and password
+Then : User clicks on Login button
+And : user is on homepage
---
+++ src/stepdefination/Login.java
@@ -0,0 +1,20 @@
+package stepdefination;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.java.en.Given;
+import cucumber.api.junit.*;
+
+
+
+public class Login 
+{
+	//@Given("^User is already on Login page$")
+	//public void user_already_login_page()
+	//{
+	
+	//}
+	
+	
+}
